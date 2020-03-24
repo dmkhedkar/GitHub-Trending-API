97e65c0a6be8b9bf533db7d38e20bfcf99c22fb8
dinesh <khedkar.dm@gmail.com>
2020-03-24 17:14:41 +0800

    Basic setup

---
+++ app/src/main/java/com/githubtrendingapi/ui/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.githubtrendingapi.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.githubtrendingapi.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
